8f69231014cfacf06b36d12bca42a16a62b0d0f1
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-03 18:29:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let participantes = [
     {
     nome: "Mayk Brito",
     email: "mayk@gmail.com",
-    dataInscrico: new Date(2024,2,22,19,20),
-    dataCheckIn: new Date(2024,2,25,22,08)
+    dataInscrico: new Date(2024, 2, 22, 19, 20),
+    dataCheckIn: new Date(2024, 2, 25, 22, 08)
     },
     {
         nome: "Fernanda Silva",

d5fa52a19e57e4751211bb22504eb47a7eead73f
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-03 18:34:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let participantes = [
     nome: "Mayk Brito",
     email: "mayk@gmail.com",
     dataInscricao: new Date(2024, 2, 22, 19, 20),
-    dataCheckIn: new Date(2024, 2, 25, 22, 8)
+    dataCheckIn: new Date(2024, 2, 25, 22, 18)
     },
     {
         nome: "Fernanda Silva",

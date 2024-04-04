de30da35d19e309d1b6fccbf54ff6e3bb3109838
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-03 23:48:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -96,5 +96,6 @@ const adicionarParticipantes =(event) => {
     event.preventDefault // nao realizar ação ao clicar com o botao
 
     const dadosDoFormulario = new FormData(event.target) // cte receberá dados ao clicar com o botao
-    alert(dadosDoFormulario.get('nome'))
+
+    alert(dadosDoFormulario.get('email'))
 }

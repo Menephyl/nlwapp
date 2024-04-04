57e6fb48e25e72fcfd66d0d1f9a2054043edf2d1
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-04 11:28:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ const adicionarParticipante =(event) => {
     participantes =[participante, ...participantes] 
     atualizarLista(participantes)
     //Limpar o form
-    event.target.querySelector('') // pegar tambem seletor de produto 
+    event.target.querySelector('[name="nome"]') // pegar tambem seletor de produto 
 }
 
 const fazerCheckIn = (event) =>{//confirmar desejo de check-in
